eac14ad6572141e746c881ba65dd4864652698a9
Edgard A. Souza <edgardsouza@falconi.com>
2018-05-21 11:25:12 -0300

    --Configuração de página de busca como default

--- testeBuscaMarvel/App_Start/RouteConfig.cs
+++ testeBuscaMarvel/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace testeBuscaMarvel
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Personagem", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
